d9d4053c4fd6cadaa14f11229956fd08dc046d44
gopikrishhnachokkalamani <gopikrishhnachokkalamani@10.87.51.250>
2018-07-26 21:59:10 -0400

    updated code with put and delete

--- src/main/java/com/springboot/StudentController.java
+++ src/main/java/com/springboot/StudentController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +32,19 @@ public class StudentController {
 
 	@PostMapping
 	public ResponseEntity<Student> insertStudentDetails(@RequestBody Student student) {
-		Student result = repository.save(student);
-		return new ResponseEntity<>(result, HttpStatus.CREATED);
+		Student responseBody = repository.save(student);
+		return new ResponseEntity<>(responseBody, HttpStatus.CREATED);
+	}
+
+	@PutMapping
+	public ResponseEntity<Student> updateStudentDetails(@RequestBody Student student) {
+		Student responseBody = repository.saveAndFlush(student);
+		return new ResponseEntity<>(responseBody, HttpStatus.OK);
+	}
+
+	@DeleteMapping(path = "{id}")
+	public ResponseEntity<String> deleteStudentDetails(@PathVariable(name = "id") String id) {
+		repository.delete(id);
+		return new ResponseEntity<String>("deleted successfully", HttpStatus.OK);
 	}
 }
